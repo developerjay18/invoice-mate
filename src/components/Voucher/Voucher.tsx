bcaf501c3aec9e76611b3cbccf6e016201f6453d
developerjay18 <jayrai3003@gmail.com>
2024-05-02 22:53:28 +0530

    adding voucher

--- src/components/Voucher/Voucher.tsx
+++ src/components/Voucher/Voucher.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Label } from '../ui/label';
 import { Input } from '@/components/ui/input';
 import { getDate } from '@/helpers/getDate';
+import { IoIosAddCircle } from 'react-icons/io';
 
 function Voucher() {
   const date = getDate();
@@ -36,8 +37,22 @@ function Voucher() {
           </div>
         </div>
 
-        <div className="grid grid-cols-3 gap-x-6 border">
-          TABLE CONTENT LOGIC PENDING...
+        <div className="flex justify-between gap-x-6 rounded">
+          <div className="w-[50%]">
+            <Label>PARTICULAR</Label>
+            <Input type="text" value={'Books and rubbers'} />
+          </div>
+          <div className="w-[23%]">
+            <Label>RS</Label>
+            <Input type="text" value={'23546'} />
+          </div>
+          <div className="w-[23%]">
+            <Label>PS</Label>
+            <Input type="text" value={'56'} />
+          </div>
+          <div className="text-5xl w-[4%] flex justify-end items-end">
+            <IoIosAddCircle />
+          </div>
         </div>
 
         <div className="grid grid-cols-3 gap-x-6">
