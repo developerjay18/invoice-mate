22d3618e8053f8b2e74573946900a93de08fd8c0
jay rai <jayrai3003@gmail.com>
2024-05-07 21:52:49 +0530

    updating database connection function - deployment

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 export async function connectDB() {
   try {
-    mongoose.connect(process.env.MONGO_URI!);
+    mongoose.connect(String(process.env.MONGO_URI!));
     const connection = mongoose.connection;
 
     connection.on('connected', () => {
