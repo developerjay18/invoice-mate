415d05c67ad56f148f93b74f70610ac3f1e8fc42
developerjay18 <jayrai3003@gmail.com>
2025-01-26 20:20:10 +0530

    updating

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['pexels.com', 'res.cloudinary.com', 'images.unsplash.com'],
+    domains: ["pexels.com", "res.cloudinary.com", "images.unsplash.com"],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  functions: {
+    "/api/extract": {
+      maxDuration: 20, // Maximum duration in seconds (default is 10)
+      memory: 512, // Memory allocation in MB (can be increased if needed)
+    },
   },
 };
 
